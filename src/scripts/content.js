52e3d65bff8a389651a609e8d7396357e25ccd07
Robert Síbek <robert.sibek@gmail.com>
2023-12-03 22:53:49 +0100

    Release version

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -15,16 +15,26 @@ if (movieTitle !== null) {
 function executeScript(title) {
     const provider_url = {
         titulky: "https://www.titulky.com/?Fulltext=",
-        opensubtitles: "https://www.imdb.com/find/?q="
+        opensubtitles: "https://www.opensubtitles.org/en/search2/sublanguageid-cze/moviename-"
     }
-    const openURL = provider_url.titulky + title;
+    let openURL = provider_url.titulky + title;
     const a = document.createElement("a");
+    const a2 = document.createElement("a");
     a.style.alignSelf = "center";
+    a2.style.alignSelf = "center";
     a.style.verticalAlign = "bottom";
+    a2.style.verticalAlign = "bottom";
     a.style.fontSize = "15px";
+    a2.style.fontSize = "15px";
     a.style.padding = "5px";
+    a2.style.padding = "5px";
     a.style.margin = "auto";
+    a2.style.margin = "auto";
     a.href = openURL;
     a.innerText = "titulky.com";
     document.querySelector(cssSelectors.titleName).insertAdjacentElement("afterend", a);
+    openURL = provider_url.opensubtitles + title;
+    a2.href = openURL;
+    a2.innerText = "opensubtitles.org";
+    document.querySelector(cssSelectors.titleName).insertAdjacentElement("afterend", a2);
 }
